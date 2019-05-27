01e2519017ad57c36ad2c600789eba6bb3959bd6
Simon Jenkins <simon@sdjenkins.com>
2019-05-27 22:09:48 +0000

    init-counters

--- src/main/java/com/sdj/general/App.java
+++ src/main/java/com/sdj/general/App.java
@@ -1,9 +1,9 @@
 package com.sdj.general;
 
-
 class App {
-  public static void main(String[] args) {
-    //TODO remove
-    Scenario.run();
-  }
+
+    public static void main(String[] args) {
+        //TODO remove
+        Scenario.run();
+    }
 }
--- src/main/java/com/sdj/general/Scenario.java
+++ src/main/java/com/sdj/general/Scenario.java
@@ -1,14 +1,29 @@
 package com.sdj.general;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 class Scenario {
 
-  private static Random rnd = new Random();
+    private static Random rnd = new Random();
 
-  public static void run() {
+    private final int n;
+    private final Set<Integer> bets = new HashSet<>();
+    private final Set<Integer> secondary = new HashSet<>();
+    private int wins;
+    private Map<Integer, Integer> winDist = new HashMap<>();;
 
 
-    System.out.println("Hello Scenario " + rnd.nextInt());
+    public Scenario() {
+
+    }
+
+    public static void run() {
+
+
+        System.out.println("Hello Scenario " + rnd.nextInt());
   }
 }
