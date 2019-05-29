4dbdfeb53430aeb690de86381345f03e7c5681e1
Simon Jenkins <simon@sdjenkins.com>
2019-05-29 12:47:24 +0000

    first-run

--- src/main/java/com/sdj/general/App.java
+++ src/main/java/com/sdj/general/App.java
@@ -1,9 +1,23 @@
 package com.sdj.general;
 
+import java.util.Arrays;
+import java.util.List;
+
 class App {
 
     public static void main(String[] args) {
-        //TODO remove
-        Scenario.run();
+        
+        List<Integer> c1 = Arrays.asList(new Integer[] {1,2,3,4,5,6,7,8,9,10,11,12});
+
+        Scenario s1 = new Scenario(1000, 6, c1);
+        s1.run();
+        System.out.printf("\nwin dist: %s", s1.winDist);
+        System.out.printf("\ndepth win dist: %s", s1.depthWinDist);
+        System.out.printf("\npre dist: %s", s1.preWinDist);
+        System.out.printf("\ndepth: %s", s1.depth);
+        System.out.printf("\ndips: %s", s1.dips);
+        System.out.printf("\nmax depth: %s", s1.maxDepth);
+        System.out.printf("\nrecoveries: %s", s1.recoveries);
+        System.out.printf("\n\n");
     }
 }
--- src/main/java/com/sdj/general/Scenario.java
+++ src/main/java/com/sdj/general/Scenario.java
@@ -1,5 +1,7 @@
 package com.sdj.general;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -10,20 +12,76 @@ class Scenario {
 
     private static Random rnd = new Random();
 
-    private final int n;
+    private final int spins;
     private final Set<Integer> bets = new HashSet<>();
     private final Set<Integer> secondary = new HashSet<>();
-    private int wins;
-    private Map<Integer, Integer> winDist = new HashMap<>();;
+    private final int waitLimit;
 
+    Map<Integer, Integer> winDist = new HashMap<>();
+    Map<Integer, Integer> preWinDist = new HashMap<>();
+    Map<Integer, Integer> depthWinDist = new HashMap<>();
+    int attempts = 1;
+    int depth = 0;
+    int maxDepth = 0;
+    int recoveries = 0;
+    int dips = 0;
 
-    public Scenario() {
+    public Scenario(int spins, int waitLimit, Collection<Integer> bets) {
 
+        this.spins = spins;
+        this.waitLimit = waitLimit;
+        this.bets.addAll(bets);
     }
 
-    public static void run() {
+    public void run() {
 
+        for (int i = 0; i < spins; i ++) {
+            int winner = rnd.nextInt(37);
+            process(winner);
+        }
+    }
 
-        System.out.println("Hello Scenario " + rnd.nextInt());
-  }
+    private static void accumulate(Map<Integer, Integer> target, Integer key) {
+
+        Integer value = target.get(key);
+        if (value == null) {
+            value = 1;
+        }
+        else {
+            value++;
+        }
+        target.put(key, value);
+    }
+
+    private void process(int winner) {
+
+        if (bets.contains(winner)) {
+            processWin();
+        }
+        else {
+            processLoss();
+        }
+        attempts++;
+    }
+
+    private void processLoss() {
+
+        accumulate(preWinDist, attempts);
+        if (attempts == waitLimit) {
+            depth++;
+            dips++;
+            maxDepth = Math.max(depth, maxDepth);
+        }
+    }
+
+    private void processWin() {
+
+        accumulate(winDist, attempts);
+        if (depth > 0) {
+            accumulate(depthWinDist, attempts);
+            depth--;
+            recoveries++;
+        }
+        attempts = 0;
+    }
 }
