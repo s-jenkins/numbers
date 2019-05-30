66c6a07c33a76ade61d42fa2ee6f96de5c17f797
Simon Jenkins <simon@sdjenkins.com>
2019-05-30 20:09:48 +0000

    analysis

--- src/main/java/com/sdj/general/App.java
+++ src/main/java/com/sdj/general/App.java
@@ -6,18 +6,11 @@ import java.util.List;
 class App {
 
     public static void main(String[] args) {
-        
-        List<Integer> c1 = Arrays.asList(new Integer[] {1,2,3,4,5,6,7,8,9,10,11,12});
 
-        Scenario s1 = new Scenario(1000, 6, c1);
+        List<Integer> c1 = Arrays.asList(new Integer[] {25, 26, 28, 29});
+
+        Scenario s1 = new Scenario(5000, 99, c1);
         s1.run();
-        System.out.printf("\nwin dist: %s", s1.winDist);
-        System.out.printf("\ndepth win dist: %s", s1.depthWinDist);
-        System.out.printf("\npre dist: %s", s1.preWinDist);
-        System.out.printf("\ndepth: %s", s1.depth);
-        System.out.printf("\ndips: %s", s1.dips);
-        System.out.printf("\nmax depth: %s", s1.maxDepth);
-        System.out.printf("\nrecoveries: %s", s1.recoveries);
-        System.out.printf("\n\n");
+        System.out.println(s1);
     }
 }
--- src/main/java/com/sdj/general/Scenario.java
+++ src/main/java/com/sdj/general/Scenario.java
@@ -1,6 +1,5 @@
 package com.sdj.general;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -10,6 +9,7 @@ import java.util.Set;
 
 class Scenario {
 
+    private enum RecoveryMode {OFF, WAITING, RECOVER_WAITING, RECOVERING}
     private static Random rnd = new Random();
 
     private final int spins;
@@ -17,14 +17,20 @@ class Scenario {
     private final Set<Integer> secondary = new HashSet<>();
     private final int waitLimit;
 
-    Map<Integer, Integer> winDist = new HashMap<>();
-    Map<Integer, Integer> preWinDist = new HashMap<>();
-    Map<Integer, Integer> depthWinDist = new HashMap<>();
+    final Map<Integer, Integer> winDist = new HashMap<>();
+    final Map<Integer, Integer> offDist = new HashMap<>();
+    final Map<Integer, Integer> preWinDist = new HashMap<>();
+    final Map<Integer, Integer> depthWinDist = new HashMap<>();
+    final Map<Integer, Integer> waitWinDist = new HashMap<>();
+
     int attempts = 1;
     int depth = 0;
     int maxDepth = 0;
     int recoveries = 0;
     int dips = 0;
+    int waits = 0;
+    int wins = 0;
+    RecoveryMode rMode = RecoveryMode.OFF;
 
     public Scenario(int spins, int waitLimit, Collection<Integer> bets) {
 
@@ -68,20 +74,99 @@ class Scenario {
 
         accumulate(preWinDist, attempts);
         if (attempts == waitLimit) {
-            depth++;
-            dips++;
-            maxDepth = Math.max(depth, maxDepth);
+            waits++;
+            switch (rMode) {
+                case OFF :
+                    rMode = RecoveryMode.WAITING;
+                    break;
+                case RECOVERING :
+                    rMode = RecoveryMode.RECOVER_WAITING;
+                    break;
+            }
         }
     }
 
     private void processWin() {
 
+        wins++;
         accumulate(winDist, attempts);
-        if (depth > 0) {
-            accumulate(depthWinDist, attempts);
-            depth--;
-            recoveries++;
+        switch (rMode) {
+            case OFF :
+                accumulate(offDist, attempts);
+                break;
+            case WAITING :
+                rMode = RecoveryMode.RECOVERING;
+                depth = 1;
+                maxDepth = Math.max(depth, maxDepth);
+                dips++;
+                break;
+            case RECOVERING :
+                accumulate(depthWinDist, attempts);
+                depth--;
+                recoveries++;
+                if (depth == 0) {
+                    rMode = RecoveryMode.OFF;
+                }
+                break;
+            case RECOVER_WAITING :
+                rMode = RecoveryMode.RECOVERING;
+                depth++;
+                dips++;
+                maxDepth = Math.max(depth, maxDepth);
+            default :
         }
         attempts = 0;
     }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n\n");
+
+        int max = winDist.keySet().stream().reduce(Integer::max).get();
+        float roll = 0;
+        for (int i = 1; i <= max; i++) {
+            Integer n = winDist.get(i);
+            n = n == null ? 0 : n;
+            float f = n;
+            roll += f;
+            float w = wins;
+            sb.append(String.format("%.0f\n", f));
+            //sb.append(String.format("%s, %.0f, %.1f, %.1f\n",
+            //    i, f, f*100/w, roll*100/w));
+        }
+        sb.append("wins: ");
+        sb.append(wins);
+        sb.append(" ");
+        sb.append(wins*100f/spins);
+        sb.append("%");
+
+        sb.append("\n");
+
+        sb.append(winDist);
+        sb.append("\n");
+
+        return sb.toString();
+    }
+
+    public String toString1() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("\n-\nwins: %s", wins));
+        sb.append(String.format("\n-\nwin dist: %s", winDist));
+        sb.append(String.format("\noff dist: %s", offDist));
+        sb.append(String.format("\ndepth win dist: %s", depthWinDist));
+        sb.append(String.format("\npre dist: %s", preWinDist));
+        sb.append(String.format("\ndips: %s", dips));
+        sb.append(String.format("\nmax depth: %s", maxDepth));
+        sb.append(String.format("\nrecoveries: %s", recoveries));
+        sb.append(String.format("\nwaits: %s", waits));
+        sb.append(String.format("\n-\nstatus: %s", rMode));
+        sb.append(String.format("\nattemps: %s\n-\n", attempts));
+
+        return sb.toString();
+    }
+
 }
